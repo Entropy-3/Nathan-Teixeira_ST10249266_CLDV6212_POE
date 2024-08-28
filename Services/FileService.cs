6287fd555e8db0592fe166bf7582727e218d3734
Nathan Teixeira <130435264+Entropy-3@users.noreply.github.com>
2024-08-28 14:15:20 +0200

    Updates nuget packages and worked on fileservice

--- Services/FileService.cs
+++ Services/FileService.cs
@@ -9,5 +9,22 @@ namespace CLDV_SEM2_POE.Services
 {
     public class FileService
     {
+
+        private readonly ShareServiceClient _shareServiceClient;
+
+        public FileService(IConfiguration configuration)
+        {
+            _shareServiceClient = new ShareServiceClient(configuration["AzureStorage:ConnectionString"]);
+        }
+
+        public async Task UploadFileAsync(string shareName, string fileName, Stream content)
+        {
+            var shareClient = _shareServiceClient.GetShareClient(shareName);
+            await shareClient.CreateIfNotExistsAsync();
+            var directoryClient = shareClient.GetRootDirectoryClient();
+            var fileClient = directoryClient.GetFileClient(fileName);
+            await fileClient.CreateAsync(content.Length);
+            await fileClient.UploadAsync(content);
+        }
     }
 }
